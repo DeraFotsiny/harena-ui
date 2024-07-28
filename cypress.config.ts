9203fb713064ec5a84a3dfc20385e87b350aa1a7
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-07-28 21:45:49 +0300

    chore: config cypress to run with default test

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  component: {
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
