d800d24a4c4cb99794f6bbb34e9bcc2d96e8480e
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-07-28 13:22:00 +0300

    chore: Add plugin for path handling

---
+++ cypress/plugins/index.js
@@ -0,0 +1,29 @@
+const wp = require('@cypress/webpack-preprocessor');
+
+module.exports = (on) => {
+  const options = {
+    webpackOptions: {
+      resolve: {
+        extensions: ['.ts', '.js'],
+        alias: {
+          '@src': require('path').resolve(__dirname, '../../src'),
+        },
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            exclude: [/node_modules/],
+            use: [
+              {
+                loader: 'ts-loader',
+              },
+            ],
+          },
+        ],
+      },
+    },
+  };
+
+  on('file:preprocessor', wp(options));
+};
