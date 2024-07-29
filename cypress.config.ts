c7631823e9e2e947e02a8a7aa8f824d2ebb4825c
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-07-29 08:50:38 +0300

    chore: add environment variable

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { defineConfig } = require("cypress");
+import dotenv from 'dotenv';
 
+dotenv.config({ path: '.env.local' });
+
+  
 module.exports = defineConfig({
   component: {
     viewportHeight: 1080,
@@ -13,7 +17,7 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on , config) {
-      // implement node event listeners here
-    },
+      config.env = process.env;
+      return config;    },
   },
 });
